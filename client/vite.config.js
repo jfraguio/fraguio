7b99992d5451f09adcc811cf31f63eeaf0ad2f22
jfraguio <jfraguio@users.noreply.github.com>
2025-09-08 19:55:35 +0200

    puerto fijo 3123

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
+    port: 3123,
   },
   build: {
     outDir: 'dist',
